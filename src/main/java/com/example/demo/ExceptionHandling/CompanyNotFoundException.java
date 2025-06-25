5c8d26a7103090e2818cf66b5892781dc81eb0a8
chiranjeevi chandu <chiranjeevichandup@gmail.com>
2025-06-25 20:22:53 +0530

    I have implemented few positive test cases and negative test cases and Rest docs and code coverage

---
+++ src/main/java/com/example/demo/ExceptionHandling/CompanyNotFoundException.java
@@ -0,0 +1,7 @@
+package com.example.demo.ExceptionHandling;
+
+public class CompanyNotFoundException extends  RuntimeException{
+    public CompanyNotFoundException(String message) {
+        super(message);
+    }
+}
