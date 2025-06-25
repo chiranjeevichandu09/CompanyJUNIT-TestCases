8e5aaa888c9a78e49cdafbfaad0e5792231cbb58
chiranjeevi chandu <chiranjeevichandup@gmail.com>
2025-06-25 20:20:31 +0530

    I have implemented few positive test cases and negative test cases and Rest docs and code coverage

--- src/main/java/com/example/demo/Entity/Company.java
+++ src/main/java/com/example/demo/Entity/Company.java
@@ -4,6 +4,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -12,19 +14,21 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-
 public class Company {
-	
-	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
-	private Integer id;
-	
-	private String companyName;
-	
-	private String companyId;
-	
-	private String companyPurpose;
-	
-	private String companyLocation;
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+
+	@NotBlank(message = "Company name is required")
+	@Size(min = 2, max = 100, message = "Company name must be between 2 and 100 characters")
+	private String companyName;
+
+	@NotBlank(message = "Company ID is required")
+	@Size(min = 5, max = 20, message = "Company ID must be between 5 and 20 characters")
+	private String companyId;
+
+	private String companyPurpose;
+
+	private String companyLocation;
 }
